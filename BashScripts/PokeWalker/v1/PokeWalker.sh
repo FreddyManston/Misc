846af6010dfdf4228dacbd9ca000a31095dd5587
FreddyManston <freddymanston@gmail.com>
2018-06-16 18:34:54 +0200

    Updated PyGame and PokeWalker folders

--- BashScripts/PokeWalker/v1/PokeWalker.sh
+++ BashScripts/PokeWalker/v1/PokeWalker.sh
@@ -5,13 +5,16 @@
 #!/bin/bash
 
 poke_starter=""
-	echo "Pick a starter Pokemon: (B)ulbasaur | (S)quirtle | (C)harmander"
-	read poke_starter
-while  [[  "$poke_starter" -eq 'B' || "$poke_starter" -eq 'S' || "$poke_starter" -eq 'C' ]]; do
+echo "Pick a starter Pokemon: (B)ulbasaur | (S)quirtle | (C)harmander"
+read poke_starter
+until test "$poke_starter" != "B"
+test "$poke_starter" != "S"
+test "$poke_starter" != "C"
+do
 	echo "Wrong input. Try Again."
 	echo "Enter: 'B' for Bulba | 'S' for Squirty | 'C' for Charry"
 	read poke_starter
 done
-echo "$poke_starter"
+echo You chose: "$poke_starter"
 
 
